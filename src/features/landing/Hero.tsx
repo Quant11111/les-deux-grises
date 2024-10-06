ae8a84cdc125ebead8605a2c37a1163bf72889e4
Quant11111 <lamagnere.quentin@gmail.com>
2024-10-06 02:13:59 +0200

    mend

--- src/features/landing/Hero.tsx
+++ src/features/landing/Hero.tsx
@@ -2,7 +2,7 @@ import { LogoSvg } from "@/components/svg/LogoSvg";
 
 export const Hero = () => {
   return (
-    <main className="relative m-auto my-12 flex min-h-screen w-full max-w-7xl items-center justify-center gap-4 bg-corporateBlue px-8 max-lg:flex-col">
+    <main className="relative m-auto my-12 flex min-h-screen w-full max-w-7xl items-center justify-center gap-4 bg-grassGreen px-8 max-lg:flex-col">
       <LogoSvg height={400} color="#E3DDD2" />
     </main>
   );
