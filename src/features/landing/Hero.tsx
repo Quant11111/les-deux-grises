d7b48ab3b97d60fa14dfc6ef8acc0c59068488a5
Quant11111 <lamagnere.quentin@gmail.com>
2024-10-07 10:47:49 +0200

    fix horizontal scrolling

--- src/features/landing/Hero.tsx
+++ src/features/landing/Hero.tsx
@@ -2,8 +2,8 @@ import { LogoSvg } from "@/components/svg/LogoSvg";
 
 export const Hero = () => {
   return (
-    <main className="relative m-auto my-12 flex min-h-screen w-full max-w-7xl items-center justify-center gap-4 bg-grassGreen px-8 max-lg:flex-col">
-      <LogoSvg height={400} color="#E3DDD2" />
+    <main className="relative m-auto my-12 flex min-h-screen max-w-screen-2xl items-center justify-center gap-4 bg-grassGreen px-8 max-lg:flex-col">
+      <LogoSvg size={400} color="#E3DDD2" />
     </main>
   );
 };
